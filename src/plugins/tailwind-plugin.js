d539eff96f42e83c0a4b28c2efcac02ad0512c40
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2025-07-28 18:42:57 +0530

    feat: build css - with USE_SIMPLE_CSS_MINIFIER

--- src/plugins/tailwind-plugin.js
+++ src/plugins/tailwind-plugin.js
@@ -3,7 +3,8 @@ module.exports = function tailwindPlugin(context, options) {
   return {
     name: 'tailwind-plugin',
     configurePostCss(postcssOptions) {
-      postcssOptions.plugins = [require('@tailwindcss/postcss')]
+      postcssOptions.plugins = [require('@tailwindcss/postcss')];
+      postcssOptions.plugins.push(require("autoprefixer"));
       return postcssOptions
     }
   }
