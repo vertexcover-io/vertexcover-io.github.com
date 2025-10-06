c1bcd853b35dd2a16e4d578a108d99a7075d8d29
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2025-10-06 12:55:32 +0530

    fix: minor copmonent

--- src/components/FlexibleNumberedList.tsx
+++ src/components/FlexibleNumberedList.tsx
@@ -7,20 +7,12 @@ interface FlexibleNumberedListProps {
 
 export default function FlexibleNumberedList({ children, startNumber = 1 }: FlexibleNumberedListProps) {
   return (
-    <ol style={{ 
-      paddingLeft: '20px',
-      margin: '16px 0',
-      lineHeight: '1.6',
-      listStyle: 'decimal',
-      listStyleType: 'decimal',
-      listStylePosition: 'outside'
-    }} start={startNumber}>
+    <ol 
+      className="pl-5 my-4 leading-relaxed list-decimal list-outside"
+      start={startNumber}
+    >
       {children.map((child, index) => (
-        <li key={index} style={{ 
-          marginBottom: '8px',
-          paddingLeft: '8px',
-          display: 'list-item'
-        }}>
+        <li key={index} className="mb-2 pl-2">
           {child}
         </li>
       ))}
